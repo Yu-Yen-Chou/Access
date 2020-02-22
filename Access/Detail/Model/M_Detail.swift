60120e1706ae1be88dcf38c6c744026f7326cc36
Eric <eagle2283429@yahoo.com.tw>
2020-02-22 11:51:23 +0800

    upload

---
+++ Access/Detail/Model/M_Detail.swift
@@ -0,0 +1,22 @@
+//
+//  M_Detail.swift
+//  Access
+//
+//  Created by Yen on 2020/2/20.
+//  Copyright © 2020 Yen. All rights reserved.
+//
+
+import Foundation
+struct M_Detail: Codable
+{
+    let login: String?
+    let id: Int?
+    let avatar_url: String?
+    let name: String?
+    let bio: String?
+    let site_admin: Bool?
+    let location: String?
+    let blog: String?
+    
+}
+
